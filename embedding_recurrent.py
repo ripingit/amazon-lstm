4cf4d6680ed572f4d40feabac6a4f440f8a60557
mh2parker <mh2parker@gmail.com>
2018-07-30 10:48:17 +0200

    adding support for multiple classes

--- embedding_recurrent.py
+++ embedding_recurrent.py
@@ -11,9 +11,18 @@ from keras.models import Sequential
 from keras.layers import Dropout, Dense, Activation, LSTM, Bidirectional, Masking, Embedding
 from sklearn.model_selection import train_test_split
 import gc
+import argparse 
 
-max_review_length = 370
-vector_dimensionality = 300
+parser = argparse.ArgumentParser(description='Specify the review length, vector dimensionality, and number of classes that you want.')
+parser.add_argument("-c", action = "store", nargs = '?', default = 2, type = int, dest = "simplification", help = "2: binary (default); 3: negative, neutral, positive; 5: 1-5 stars")
+parser.add_argument("-d", action = "store", nargs = '?', default = 2, type = int, dest = "dimensionality", help = "specify vector dimensionality ")
+parser.add_argument("-r", action = "store", nargs = '?', default = 2, type = int, dest = "review_length", help = "max_review_length (generate reviews of this length using get_word_mapping)")
+args = parser.parse_args()
+
+
+max_review_length = args.review_length
+vector_dimensionality = args.dimensionality
+simplification_level = args.simplification
 
 embedding_matrix = np.load(str(vector_dimensionality) + "d_vocab_vector_matrix.npz")
 embedding_matrix = embedding_matrix[embedding_matrix.keys()[0]]
@@ -22,13 +31,16 @@ embedding_matrix = embedding_matrix[embedding_matrix.keys()[0]]
 x_unsplit = np.load(str(vector_dimensionality)+ "d_" + str(max_review_length) + "l_indexed_unsplit.npz") 
 x_unsplit = x_unsplit[x_unsplit.keys()[0]]
 
-y_file = open("y_unsplit_balanced.txt", "r", encoding = "utf-8")
+y_file = open("y_"+str(simplification_level) + "_way_balanced.txt", "r", encoding = "utf-8")
 y_unsplit = np.array([int(line.strip()) for line in y_file])
+
+y_unsplit = y_unsplit if simplification_level == 2 else to_categorical(y_unsplit)
 
 x_train, x_test, y_train, y_test = train_test_split(x_unsplit, y_unsplit, test_size=0.2)
 del x_unsplit
 
-
+loss_type = "binary_crossentropy" if simplification_level == 2 else "categorical_crossentropy"
+number_of_units = 1 if simplification_level == 2 else simplification_level
 
 ####do the same for a few more models
 #the input dimensionality is any number of samples, each containing a sequence of max_review_length 300-d vectors
@@ -39,7 +51,7 @@ model.add(Masking(mask_value = 0.0, input_shape = (max_review_length, vector_dim
 model.add(Bidirectional(LSTM(64, activation = "tanh", dropout = 0.5, recurrent_dropout = 0.5)))
 model.add(Dense(32, activation = "relu"))
 model.add(Dropout(0.3))
-model.add(Dense(1, activation = "sigmoid"))
+model.add(Dense(number_of_units, activation = "sigmoid"))
 model.compile(optimizer = "adam", loss = "binary_crossentropy", metrics = ["accuracy"])
 model.fit(x_train, y_train, epochs = 30, validation_split = 0.5, batch_size = 200)
 score = model.evaluate(x_test, y_test, batch_size = 200)
